9f92ccf5cd3d2f75b4217dfed291d4db2bc4e996
Anitha Thanam <anitha.thanam@outlook.com>
2019-03-14 17:37:25 -0700

    Match Pattern, Hash table using chaining

---
+++ Chaining/Program.cs
@@ -0,0 +1,236 @@
+﻿/*
+In this task your goal is to implement a hash table with lists chaining. You are already given the
+number of buckets 𝑚 and the hash function. It is a polynomial hash function
+          𝑆−1
+ℎ(𝑆) = ⎛ ⎝ |Σ︁| 𝑆[𝑖]𝑥𝑖 mod 𝑝 ⎞ ⎠ mod 𝑚,
+          𝑖=0
+where 𝑆[𝑖] is the ASCII code of the 𝑖-th symbol of 𝑆, 𝑝 = 1 000 000 007 and 𝑥 = 263. Your program
+should support the following kinds of queries:
+∙ add string — insert string into the table. If there is already such string in the hash table, then
+just ignore the query.
+∙ del string — remove string from the table. If there is no such string in the hash table, then
+just ignore the query.
+∙ find string — output “yes" or “no" (without quotes) depending on whether the table contains
+string or not.
+∙ check 𝑖 — output the content of the 𝑖-th list in the table. Use spaces to separate the elements of
+the list. If 𝑖-th list is empty, output a blank line.
+When inserting a new string into a hash chain, you must insert it in the beginning of the chain.
+Input Format. There is a single integer 𝑚 in the first line — the number of buckets you should have. The
+next line contains the number of queries 𝑁. It’s followed by 𝑁 lines, each of them contains one query
+in the format described above.
+Constraints. 1 ≤ 𝑁 ≤ 105; 𝑁
+5 ≤ 𝑚 ≤ 𝑁. All the strings consist of latin letters. Each of them is non-empty
+and has length at most 15.
+Output Format. Print the result of each of the find and check queries, one result per line, in the same
+order as these queries are given in the input.
+Memory Limit. 512Mb.
+5
+Sample 1.
+Input:
+5
+12
+add world
+add HellO
+check 4
+find World
+find world
+del world
+check 4
+del HellO
+add luck
+add GooD
+check 2
+del good
+Output:
+HellO world
+no
+yes
+HellO
+GooD luck
+Explanation:
+The ASCII code of ’w’ is 119, for ’o’ it is 111, for ’r’ it is 114, for ’l’ it is 108, and for ’d’ it is 100. Thus,
+ℎ(“world") = (119+111×263+114×2632 +108×2633 +100×2634 mod 1 000 000 007) mod 5 = 4.
+It turns out that the hash value of 𝐻𝑒𝑙𝑙𝑂 is also 4. Recall that we always insert in the beginning
+of the chain, so after adding “world" and then “HellO" in the same chain index 4, first goes “HellO"
+and then goes “world". Of course, “World" is not found, and “world" is found, because the strings
+are case-sensitive, and the codes of ’W’ and ’w’ are different. After deleting “world", only “HellO" is
+found in the chain 4. Similarly to “world" and “HellO", after adding “luck" and “GooD" to the same
+chain 2, first goes “GooD" and then “luck".
+Sample 2.
+Input:
+4
+8
+add test
+add test
+find test
+del test
+find test
+find Test
+add Test
+find Test
+Output:
+yes
+no
+no
+yes
+Explanation:
+Adding “test" twice is the same as adding “test" once, so first find returns “yes". After del, “test" is
+6
+no longer in the hash table. First time find doesn’t find “Test” because it was not added before, and
+strings are case-sensitive in this problem. Second time “Test” can be found, because it has just been
+added.
+Sample 3.
+Input:
+3
+12
+check 0
+find help
+add help
+add del
+add add
+find add
+find del
+del del
+find del
+check 0
+check 1
+check 2
+Output:
+no
+yes
+yes
+no
+add help
+Explanation:
+Note that you need to output a blank line when you handle an empty chain. Note that the strings
+stored in the hash table can coincide with the commands used to work with the hash table.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chaining
+{
+    class Bucket
+    {
+        internal string data;
+        internal Bucket next;
+
+        internal Bucket(string s)
+        {
+            data = s;
+        }
+        
+    }
+
+    class Program
+    {
+        const int MaxStringLength = 15;
+        const int prime = 1000000007;
+        const int x = 263;
+        static Bucket[] buckets;
+        
+        static int Hash(string s, int m)
+        {
+            long result = 0;
+            for(int i=s.Length-1;i>=0;i--)
+            {
+                result = ((result*x) + (int)s[i]) % prime;
+            }
+
+            return (int)result % m;
+        }
+
+        static bool Find(string s, int hash)
+        {
+            Bucket tmpBucket;
+            for(tmpBucket=buckets[hash]; tmpBucket!=null && tmpBucket.data!=s; tmpBucket=tmpBucket.next);
+            if(tmpBucket == null)
+                return false;
+            else
+                return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            List<string> results = new List<string>();
+            string check;
+            string[] line = new string[2];
+            int hash;
+            Bucket tmpBucket;
+            buckets = new Bucket[m];
+            
+            for(int l=1;l<=n;l++)
+            {
+                line = Console.ReadLine().Split(' ');
+                hash = Hash(line[1], m);
+                switch (line[0])
+                {
+                    case "add":
+                        if(!Find(line[1], hash))
+                        {
+                            tmpBucket = new Bucket(line[1]);
+                            tmpBucket.next = buckets[hash];
+                            buckets[hash] = tmpBucket;
+                        }
+                        break;
+                    case "del":
+                        if(buckets[hash] == null)
+                        {
+                            break;
+                        }
+                        else if(buckets[hash].data == line[1])
+                        {
+                            buckets[hash] = buckets[hash].next;
+                            break;
+                        }
+                        for(tmpBucket=buckets[hash]; tmpBucket.next!=null && tmpBucket.next.data!=line[1]; tmpBucket=tmpBucket.next);
+                        if(tmpBucket.next!=null && tmpBucket.next.data == line[1])
+                        {
+                            tmpBucket.next = tmpBucket.next.next;
+                        }
+                        break;
+                    case "find":
+                        if(Find(line[1], hash))
+                            results.Add("yes");
+                        else
+                            results.Add("no");
+                        break;
+                    case "check":
+                        tmpBucket = buckets[int.Parse(line[1])];
+                        check = "";
+                        while(tmpBucket != null)
+                        {
+                            check += tmpBucket.data + ' ';
+                            tmpBucket = tmpBucket.next;
+                        }
+                        results.Add(check);
+                        break;
+                }
+                /*
+                int c=0;
+                foreach(Bucket b in buckets)
+                {
+                    tmpBucket = b;
+                    Console.Write(c.ToString() + ' ');
+                    while(tmpBucket != null)
+                    {
+                        Console.Write(tmpBucket.data + ' ');
+                        tmpBucket = tmpBucket.next;
+                    }
+                    Console.WriteLine();
+                    c++;
+                }
+                */
+            }
+            
+            foreach(string r in results)
+            {
+                Console.WriteLine(r);
+            }
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ MatchPattern/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+In this problem your goal is to implement the Rabin–Karp’s algorithm for searching the given pattern
+in the given text.
+Input Format. There are two strings in the input: the pattern 𝑃 and the text 𝑇.
+Constraints. 1 ≤ |𝑃| ≤ |𝑇| ≤ 5 · 105. The total length of all occurrences of 𝑃 in 𝑇 doesn’t exceed 108. The
+pattern and the text contain only latin letters.
+Output Format. Print all the positions of the occurrences of 𝑃 in 𝑇 in the ascending order. Use 0-based
+indexing of positions in the the text 𝑇.
+Time Limits. C: 1 sec, C++: 1 sec, Java: 5 sec, Python: 5 sec. C#: 1.5 sec, Haskell: 2 sec, JavaScript:
+3 sec, Ruby: 3 sec, Scala: 3 sec.
+Memory Limit. 512Mb.
+Sample 1.
+Input:
+aba
+abacaba
+Output:
+0 4
+Explanation:
+The pattern 𝑎𝑏𝑎 can be found in positions 0 (abacaba) and 4 (abacaba) of the text 𝑎𝑏𝑎𝑐𝑎𝑏𝑎.
+Sample 2.
+Input:
+Test
+testTesttesT
+Output:
+4
+Explanation:
+Pattern and text are case-sensitive in this problem. Pattern 𝑇𝑒𝑠𝑡 can only be found in position 4 in
+the text 𝑡𝑒𝑠𝑡𝑇 𝑒𝑠𝑡𝑡𝑒𝑠𝑇 .
+Sample 3.
+Input:
+aaaaa
+baaaaaaa
+Output:
+1 2 3
+Explanation:
+Note that the occurrences of the pattern in the text can be overlapping, and that’s ok, you still need
+to output all of them. 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MatchPattern
+{
+    class Program
+    {
+        const int MaxStringLength = 15;
+        const int prime = 1000000007;
+        const int x = 263; //random number between 1 and prime-1
+
+        static long PolyHash(string s)
+        {
+            long result = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                result = ((result * x) + (int)s[i]) % prime;
+            }
+
+            return result;
+        }
+
+        static long[] PreComputeHashes(string text, int patternLength)
+        {
+            string s = text.Substring(text.Length-patternLength);
+            long[] hash = new long[text.Length - patternLength + 1];
+            hash[text.Length - patternLength] = PolyHash(s);
+            int y = 1;
+            for (int i = 1; i <= patternLength; i++)
+                y = (y * x) % prime;
+            for(int i = text.Length - patternLength - 1; i>=0;i--)
+            {
+                hash[i] = (x * hash[i+1] + (int)text[i] - y * (int)text[i + patternLength]) % prime;
+            }
+            return hash;
+        }
+
+        static List<int> RabinKarp(string text, string pattern)
+        {
+            List<int> result = new List<int>();
+            long pHash = PolyHash(pattern);
+            long[] hash = PreComputeHashes(text, pattern.Length);
+            for(int i=0;i<=text.Length-pattern.Length;i++)
+            {
+                if (pHash != hash[i])
+                    continue;
+                if (pattern == text.Substring(i, pattern.Length))
+                    result.Add(i);
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string pattern = Console.ReadLine();
+            string text = Console.ReadLine();
+            foreach(int p in RabinKarp(text, pattern))
+            {
+                Console.Write(p.ToString() + ' ');
+            }
+            Console.ReadLine();
+        }
+    }
+}
